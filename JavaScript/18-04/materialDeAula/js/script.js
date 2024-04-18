b867d2b26adcf9a9d07fbcf8372409e3b16a2855
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-04-18 09:53:33 -0300

    javascript

---
+++ JavaScript/18-04/materialDeAula/js/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+//input box
+
+//prompt = recebe texto
+//window.prompt("Quantos anos você se chama?");
+
+//alert = manda uma mensagem
+//window.alert("FRONT NÃO É TOP!");
+
+//confirm = pergunta podendo sair verdadeiro o falso
+//window.confirm("Deseja baixa arquivo novamente?")
+
+var melancia = 'Test';
+var javalee = 2;
+var ligado = true;
+var desligado = false;
+
+//escrever typeof no console teste o tipo de variável
