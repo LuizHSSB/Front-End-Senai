5e147f1da6cb8a1cc4116e44560b80127a95eee8
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-04-23 11:50:52 -0300

    js if else

--- JavaScript/23-04/materialDeAula/index.js
+++ JavaScript/23-04/materialDeAula/index.js
@@ -1,25 +1,47 @@
-/* 
-Estrutura básica
+function ifElse(){
 
-if (){
-    alert("Estou no if")
+//Estrutura básica
+
+if (true){
+    console.log("Estou no if")
 }else{
-    alert("Estou no Else")
+    console.log("Estou no Else")
 }
 
-*/
+//Verifica idade
+//Input - Idade
+//Output - resposta
+//Verifica se o usuário é maior de idade
 
+window.alert("Seja bem vindo(a)")
+let userAge = parseInt(window.prompt("Qual a sua idade?"))
 
-function ifElse(){
-    
-
-
+if (userAge > 17){
+    window.alert(`Você tem ${userAge} anos, logo é maior de idade!`)
+    let resposta = document.querySelector("#resposta")
+    resposta.innerHTML = `<h3 style="color:red;"> ${userAge} </h3>`
+}else{
+    window.alert(`Você tem ${userAge} anos, logo é menor de idade!`)
+    location.href = "https://youtubekids.com"
+}
 }
 
 function switchCase(){
-
-
-
+    window.alert("Lojinha Brasileiras")
+    let opcao = window.prompt("Escolha a sua opção: A - Pen Drive ; B - Celular ; C - Carvão Vegetal")
+    switch(opcao){
+        case "A":
+            window.alert("Pen Drive adicionado ao carrinho!")
+            break
+        case "B":
+            window.alert("Celular adicionado ao carrinho!")
+            break
+        case "C":
+            window.alert("Carvão vegetal adicionado ao carrinho!")
+            break
+        default:
+        window.alert("Inválido")
+    } 
 }
 
 function atividade(){
