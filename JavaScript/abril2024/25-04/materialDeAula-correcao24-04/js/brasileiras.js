99705900d9e62ab7be5e588d988f2df04dccdf32
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-04-25 11:45:19 -0300

    25-04

---
+++ JavaScript/abril2024/25-04/materialDeAula-correcao24-04/js/brasileiras.js
@@ -0,0 +1,24 @@
+//document.getElementById("valorTotal"); fazem a mesma coisa;
+//document.querySelector("#valorTotal"); fazem a mesma coisa, porém esse é mais prático por nos dar a possibilidade de utilizar id, class ou tag;
+//document.querySelectorAll("a"); <- seleciona todos os links;
+
+var valorTotal = 0;
+
+function atualizarValorTotal(){
+    var valorTotalHTML = document.querySelector("#valorTotal");
+    valorTotalHTML.innerHTML = `Valor Total: R$ ${valorTotal.toFixed(2)}`;
+}
+
+function adicionarValorTotal(valorItem){
+    valorTotal += valorItem; // -> valorTotal = valorTotal + valorItem
+    atualizarValorTotal()
+
+}
+
+function removerValorTotal(valorItem){
+    valorTotal += valorItem; // -> valorTotal = valorTotal + valorItem
+    removerValorTotal()
+    if(valorTotal < 0){
+        valorTotal = 0
+    }
+}
---
+++ JavaScript/abril2024/25-04/materialDeAula-correcao24-04/js/index.js
@@ -0,0 +1,48 @@
+const login = "ADM";
+const senha = "ADM";
+
+function atividade1(){
+    window.alert("Bem vindo ao nosso site! Faça o login para prosseguir:");
+    var loginDig = window.prompt("Login:");
+    var senhaDig = window.prompt("Senha:");
+        while (login != loginDig || senha != senhaDig){
+            window.alert("Senha incorreta, digite novamente:");
+            var loginDig = window.prompt("Login:");
+            var senhaDig = window.prompt("Senha:");
+        }
+    window.alert("Login realizado com sucesso!") ;       
+}
+
+function Atividade1(){
+    window.alert("Bem vindo ao nosso site! Faça o login para prosseguir:");
+    var usuarioCorreto = false;
+    var tentativas = 0;
+
+    while (usuarioCorreto == false){
+        var loginDig = window.prompt("Login:");
+        var senhaDig = window.prompt("Senha:"); 
+            
+            if(login != loginDig || senha != senhaDig){
+                window.alert("Senha incorreta, digite novamente:");
+                tentativas++;
+                window.alert(`Número de tentativas (Max de 3): ${tentativas}`);
+                
+                if(tentativas === 3){
+                    window.alert("Número de tentativas excedido! Tente novamente mais tarde");
+                    break;
+                }
+
+            }else{
+                window.alert("Login realizado com sucesso!");
+                usuarioCorreto = true;
+            }
+    }
+}
+
+
+
+
+
+
+
+
