91721a57d6b11212bffa3e3ae35433ea1c3db773
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-05-10 17:13:58 -0300

    att

---
+++ JavaScript/maio2024/10-05/atividade/script/index.js
@@ -0,0 +1,11 @@
+$("#atividade1").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-none")
+    $("#att1").addClass("d-flex")
+    $("#att2").removeClass("d-flex")
+    $("#att2").addClass("d-none")
+
+});
