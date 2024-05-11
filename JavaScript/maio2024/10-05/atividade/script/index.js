06a8fdf2c3b2d8abe3ffe6537f316a4f8fb33933
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-05-11 02:19:51 -0300

    atividade

--- JavaScript/maio2024/10-05/atividade/script/index.js
+++ JavaScript/maio2024/10-05/atividade/script/index.js
@@ -1,11 +1,55 @@
-$("#atividade1").on("click",function(){
-    //Questões visuais (Css/BootStrap)
-    $("#atividades").removeClass("align-items-center")
-    $("#atividades").removeClass("h-100vh")
-    $("#atividades").addClass("mt-4")
-    $("#att1").removeClass("d-none")
-    $("#att1").addClass("d-flex")
-    $("#att2").removeClass("d-flex")
-    $("#att2").addClass("d-none")
+//atividade 1
+
+$("#at1").on("click",function(){
+
+    //inicio da resolução
+    $("#resp1").removeClass("d-none")
+    $("#resp1").addClass("d-flex")
+
+    $("#resp2").removeClass("d-flex")
+    $("#resp2").addClass("d-none")
+
+    $('#submitTabuada').on('click',function(){
+        document.querySelector("#resposta1").innerHTML = ``
+
+        for(let contador = 1;contador <= 10;contador++){
+            document.querySelector("#resposta1").innerHTML += `
+                <p>${document.querySelector("#inputNum").value} x ${contador} = ${document.querySelector("#inputNum").value*contador}</p>
+                `
+        }
+    });
+});
+
+$("#advancedMode").on("click",function(){
+    document.querySelector("#resposta1").innerHTML = ``
+
+        for(let contador = 1;contador <= 100;contador++){
+            document.querySelector("#resposta1").innerHTML += `
+                <p>${document.querySelector("#inputNum").value} x ${contador} = ${document.querySelector("#inputNum").value*contador}</p>
+                `
+        }
+});
+
+$("#crazyMode").on("click",function(){
+    document.querySelector("#resposta1").innerHTML = ``
+
+        for(let contador = 1;contador <= 1000;contador++){
+            document.querySelector("#resposta1").innerHTML += `
+                <p>${document.querySelector("#inputNum").value} x ${contador} = ${document.querySelector("#inputNum").value*contador}</p>
+                `
+        }
+});
+
+//atividade 2
+
+$("#switchTheme").on("click",function(){
+    if(document.querySelector("#switchTheme").checked){
+        document.querySelector("body").setAttribute("data-bs-theme","light")
+        document.querySelector("#switchName").innerHTML = `Light Theme`
+    }else{
+        document.querySelector("body").setAttribute("data-bs-theme","dark")
+        document.querySelector("#switchName").innerHTML = `Dark Theme`
+    }
 
 });
+
